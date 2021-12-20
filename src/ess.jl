a2535b4870326d4d17079550ba8086119f547239
Miguel Biron <miguelbl9@gmail.com>
2021-12-19 21:23:32 -0800

    more info in readme

--- src/ess.jl
+++ src/ess.jl
@@ -1,9 +1,5 @@
 ###############################################################################
 # effective sample size calculations
-# code ported from the R package mcmcse:
-#     James M. Flegal, John Hughes, Dootika Vats, Ning Dai, Kushagra Gupta,
-#     and Uttiya Maji. (2021). mcmcse: Monte Carlo Standard Errors for
-#     MCMC. R package version 1.5-0. Riverside, CA, and Kanpur, India.
 ###############################################################################
 
 # univariate case
