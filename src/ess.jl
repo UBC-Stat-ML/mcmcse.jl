d3c9bad48912d27a48a0abf8280e3f3ebdfe5d94
Miguel Biron <miguelbl9@gmail.com>
2021-12-19 21:26:01 -0800

    changed lambda symbol in ess

--- src/ess.jl
+++ src/ess.jl
@@ -4,7 +4,7 @@
 
 # univariate case
 function ess(x::AbstractVector{<:Real};kwargs...)
-    lambda = var(x)                  # estimate variance under stationary distribution 
-    σ²     = mcvar(x;kwargs...)[1,1] # estimate asymptotic variance of MCMC estimate 
-    return length(x)*lambda/σ²
+    λ  = var(x)                  # estimate variance under stationary distribution 
+    σ² = mcvar(x;kwargs...)[1,1] # estimate asymptotic variance of MCMC estimate 
+    return length(x)*λ/σ²
 end
